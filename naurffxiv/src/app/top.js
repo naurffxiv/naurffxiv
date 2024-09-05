489dd5bea916370171198c04ec863809ccfbe3dc
Claire <nicholas.mellinger@outlook.com>
2024-09-05 11:08:48 -0400

    Add ultimate page template

---
+++ naurffxiv/src/app/top.js
@@ -0,0 +1,29 @@
+export default function DisplayDSR() {
+	return (
+		<>
+			<title>NAUR TOP Strats and Resources</title>
+				<h1>Strats By Phase</h1>
+					<h2>Phase X: Name</h2>
+						<a href="https://exmaple.com" target="_blank">Toolbox/Raidplan Exmaple</a><br></br>
+						<em>Notes go here.</em><br></br>
+				<h1>General Resources</h1>
+					<h2>Resource</h2>
+						<a href="https://exmaple.com" target="_blank">Resource Example</a><br></br>
+					<h2>Arena Markers</h2>
+						<code>&#123;"Name":"UWU","MapID":539,"A":&#123;"X":100.0,"Y":0.0,"Z":93.3,"ID":0,"Active":true&#125;,"B":&#123;"X":106.7,"Y":0.0,"Z":100.0,"ID":1,"Active":true&#125;,"C":&#123;"X":100.0,"Y":0.0,"Z":106.7,"ID":2,"Active":true&#125;,"D":&#123;"X":93.3,"Y":0.0,"Z":100.0,"ID":3,"Active":true&#125;,"One":&#123;"X":107.3,"Y":0.0,"Z":107.3,"ID":4,"Active":true&#125;,"Two":&#123;"X":100.0,"Y":0.0,"Z":81.0,"ID":5,"Active":true&#125;,"Three":&#123;"X":100.0,"Y":0.0,"Z":100.0,"ID":6,"Active":true&#125;,"Four":&#123;"X":87.0,"Y":0.0,"Z":87.0,"ID":7,"Active":true&#125;&#125;</code>
+				<h1>Visual Guides and POVs</h1>
+					<h2>Tank PoVs</h2>
+						<a href="https://exmaple.com" target="_blank">POV Example</a><br></br>
+					<h2>Healer POVs</h2>
+						<a href="https://exmaple.com" target="_blank">POV Example</a><br></br>					
+					<h2>Melee DPS POVs</h2>
+						<a href="https://exmaple.com" target="_blank">POV Example</a><br></br>	
+					<h2>Phys Ranged DPS POVs</h2>
+						<a href="https://exmaple.com" target="_blank">POV Example</a><br></br>					
+					<h2>Magical Ranged DPS POVs</h2>
+						<a href="https://exmaple.com" target="_blank">POV Example</a><br></br>	
+					<h2>Mechanic POVs</h2>
+						<a href="https://exmaple.com" target="_blank">POV Example</a><br></br>		
+		</>
+	);
+}
