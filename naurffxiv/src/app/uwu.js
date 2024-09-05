afa7322442cede81e6d86e654cc18988ca4942e8
Claire <nicholas.mellinger@outlook.com>
2024-09-05 10:52:58 -0400

    Format UWU content

--- naurffxiv/src/app/uwu.js
+++ naurffxiv/src/app/uwu.js
@@ -1,53 +1,49 @@
 export default function DisplayUWU() {
 	return (
 		<>
-		<h1>UWU Strats and Resources</h1>
-			<h2>Table of Contents</h2>
-			UWU Strats by Phases<br></br>
-			General Fight Resources<br></br>
-			UWU Visual Guides and POVs<br></br>
-				<h3>UWU Strats By Phase</h3>
-					<h4>Phase 1: Garuda</h4>
+			<title>NAUR UWU Strats and Resources</title>
+				<h1>Strats By Phase</h1>
+					<h2>Phase 1: Garuda</h2>
 						<a href="https://ff14.toolboxgaming.space/?id=359535529826261&preview" target="_blank">Garuda Toolbox</a><br></br>
 						<em>Please note that the caster goes east and the off-tank goes west to take Mesohigh tethers.</em><br></br>
-					<h4>Phase 2: Ifrit</h4>
+					<h2>Phase 2: Ifrit</h2>
 						<a href="https://ff14.toolboxgaming.space/?id=562530446784261&preview" target="_blank">Ifrit Toolbox</a><br></br>
 						<em>Ifrit Dashes can be skipped with sufficiently high DPS.</em>	
-					<h4>Phase 3: Titan</h4>
+					<h2>Phase 3: Titan</h2>
 						<a href="https://ff14.toolboxgaming.space/?id=463534803605261&preview" target="_blank">Titan Toolbox</a><br></br>
 						<em>Party Finder groups in North America and Europe expect the use of an "automarker" (AM, "Allagan Melon") third party tool to simplify the Titan Gaols mechanic. There are Terms of Service-friendly ways of resolving this mechanic (e.g., self-marking macros), although such use is extremely rare in Party Finder and more suitable in a static environment.</em><br></br>
-					<h4>Intermission</h4>
+					<h2>Intermission</h2>
 						<a href="https://ff14.toolboxgaming.space/?id=430631425646261&preview" target="_blank">Intermission Toolbox</a><br></br>
-					<h4>Phase 4: Ultima</h4>
+					<h2>Phase 4: Ultima</h2>
 						<a href="https://ff14.toolboxgaming.space/?id=530635345646261&preview" target="_blank">Predation Toolbox</a><br></br>
 						<a href="https://ff14.toolboxgaming.space/?id=930637786646261&preview" target="_blank">Annihilation Toolbox </a><br></br>
 						<a href="https://ff14.toolboxgaming.space/?id=830635036646261&preview" target="_blank">Suppression Toolbox</a><br></br>
 						<em>Tanks left, party right on aetheric booms.</em><br></br>
-				<h3>General fight resources</h3>
-					<h4>Sample Mitigation Sheet</h4>
+				<h1>General Resources</h1>
+					<h2>Sample Mitigation Sheet</h2>
 						<a href="https://docs.google.com/spreadsheets/d/1V-FXFOW8pj87DtqT4KpKSbu3AZYI4diSjH1F-FyNt_o/edit?usp=sharing" target="_blank">Sample Mitigation Sheet</a><br></br>
-						<h4>Sample Tank Mitigation Sheet</h4>
+					<h2>Sample Tank Mitigation Sheet</h2>
 						<a href="https://docs.google.com/spreadsheets/d/13dpeUEBHFpLxX38gjC8oh1xEB4F7xBECI9BvycJEsfk/edit#gid=831820397" target="_blank">Sample Tank Mitigation Sheet</a><br></br>
-						<h4>Titan Gaols Cheat Sheet</h4>
+					<h2>Titan Gaols Cheat Sheet</h2>
 						<a href="https://github.com/naurffxiv/assets/blob/main/Toolbox%20Resources/UWU/TitanGaolsCheatSheet.png?raw=true" target="_blank">Titan Gaols Cheat Sheet</a><br></br>
-						<h4>Arena Markers</h4>
+					<h2>Arena Markers</h2>
 						<code>&#123;"Name":"UWU","MapID":539,"A":&#123;"X":100.0,"Y":0.0,"Z":93.3,"ID":0,"Active":true&#125;,"B":&#123;"X":106.7,"Y":0.0,"Z":100.0,"ID":1,"Active":true&#125;,"C":&#123;"X":100.0,"Y":0.0,"Z":106.7,"ID":2,"Active":true&#125;,"D":&#123;"X":93.3,"Y":0.0,"Z":100.0,"ID":3,"Active":true&#125;,"One":&#123;"X":107.3,"Y":0.0,"Z":107.3,"ID":4,"Active":true&#125;,"Two":&#123;"X":100.0,"Y":0.0,"Z":81.0,"ID":5,"Active":true&#125;,"Three":&#123;"X":100.0,"Y":0.0,"Z":100.0,"ID":6,"Active":true&#125;,"Four":&#123;"X":87.0,"Y":0.0,"Z":87.0,"ID":7,"Active":true&#125;&#125;</code>
-				<h3>UWU Visual Guides and POVs</h3>
-					<h4>Tank PoVs</h4>
+				<h1>Visual Guides and POVs</h1>
+					<h2>Tank PoVs</h2>
 						<a href="https://youtu.be/tbBEUF3ffeI?si=gP_lLznj9i2JdyM-" target="_blank">Dark Knight - Main Tank</a><br></br>
-					<h4>Healer PoVs</h4>
+					<h2>Healer PoVs</h2>
 						<a href="https://www.twitch.tv/videos/2153617369" target="_blank">White Mage - No Searing Wind</a><br></br>
 						<a href="https://www.twitch.tv/videos/2153690827" target="_blank">Astrologian - No Searing Wind</a><br></br>
 						<a href="https://www.twitch.tv/videos/2153686831" target="_blank">Sage - Searing Wind on Ifirt and Ultima</a><br></br>
 						<a href="https://www.twitch.tv/videos/2153692504" target="_blank">Scholar - Searing Wind on Ifirt and Ultima</a><br></br>
-					<h4>Melee DPS PoVs</h4>
+					<h2>Melee DPS PoVs</h2>
 						<a href="https://www.youtube.com/watch?v=XtDHObmvBy4" target="_blank">Ninja - First Cleanse and SE Nail</a><br></br>
-					<h4>Phys Ranged DPS PoVs</h4>
+					<h2>Phys Ranged DPS PoVs</h2>
 						<a href="https://www.youtube.com/watch?v=CEUv-iv57H8" target="_blank">Dancer - West Eruption Baits</a><br></br>
-					<h4>Magical Ranged DPS PoVs</h4>
+					<h2>Magical Ranged DPS PoVs</h2>
 						<a href="https://www.youtube.com/watch?v=DS5HDRcJYaQ" target="_blank">Summoner - East Eruption Baits</a><br></br>
 						<a href="https://youtu.be/D1sciQSLxZY" target="_blank">Red Mage - West Eruption Baits</a><br></br>
-					<h4>Mechanic POVs</h4>
+					<h2>Mechanic POVs</h2>
 						<a href="https://youtu.be/pZ6FPoVkbRY" target="_blank">Quick guide to Ifrit Dashes (Z-Order)</a><br></br>
 						<a href="https://www.youtube.com/watch?v=vKtc2LnhOkw" target="_blank">Titan Gaols - 2 POV</a><br></br>
 						<a href="https://www.youtube.com/watch?v=d08-CA5zS1c" target="_blank">Predation Quick Guide</a><br></br>
