6e0d40c0f7835bff49627e208b2f56a56694e319
Claire <nicholas.mellinger@outlook.com>
2024-09-10 15:22:32 -0400

    Replace uwu.js body

--- naurffxiv/src/app/page.js
+++ naurffxiv/src/app/page.js
@@ -1,11 +1,12 @@
 import react from "react";
 import { Container } from "@mui/material";
+import DisplayUWU from "./uwu.js";
 
 
 export default function Home() {
   return (
 	<Container sx={{ height: "100vh"}}>
-    <h1>Home</h1>
+		<DisplayUWU />
 	</Container>
   );
 }
