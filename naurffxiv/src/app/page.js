254cc773f746f80084006049e58aad772ce8a5e6
GitButler <gitbutler@gitbutler.com>
2024-09-23 16:57:53 -0700

    GitButler WIP Commit

--- naurffxiv/src/app/page.js
+++ naurffxiv/src/app/page.js
@@ -1,12 +1,18 @@
 import react from "react";
 import { Container } from "@mui/material";
+import NavBar from "./navigation.js";
 import DisplayUWU from "./uwu.js";
 
 
 export default function Home() {
   return (
-	<Container sx={{ height: "100vh"}}>
+	<>
+	<Container sx={{ maxHeight: "10vh"}}>
+		<NavBar />
+	</Container>
+	<Container sx={{ maxWidth: "10vh"}}>
 		<DisplayUWU />
 	</Container>
+	</>
   );
 }
