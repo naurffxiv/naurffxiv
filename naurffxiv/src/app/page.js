dd4c8183374ea652386ac26e6dd0774f1b28418d
Mera <krasnovian7.pub@gmail.com>
2024-09-23 17:25:39 -0700

    remove md imports, set app to functional state

--- naurffxiv/src/app/page.js
+++ naurffxiv/src/app/page.js
@@ -1,12 +1,11 @@
 import react from "react";
 import { Container } from "@mui/material";
-import UWU from "./ultimates.js";
 
 
 export default function Home() {
   return (
 	<Container sx={{ height: "100vh"}}>
-		<DisplayUWU />
+		<p>Test</p>
 	</Container>
   );
 }
--- naurffxiv/src/app/ultimates.js
+++ naurffxiv/src/app/ultimates.js
@@ -8,49 +8,49 @@ function convertToMarkdown(filename) {
 }
 
 
-export default function UWU() {
-	markdown = convertToMarkdown("uwu.md");
-
-	return (
-		<p>Placeholder: Put uwu resources here</p>
-	);
-}
-
-
-export default function UCOB() {
-	markdown = convertToMarkdown("ucob.md");
-
-	return (
-		<p>Placeholder: Put ucob resources here</p>
-	);
-}
-
-
-export default function TEA() {
-	markdown = convertToMarkdown("tea.md");
-
-	return (
-		<p>Placeholder: Put tea resources here</p>
-	);
-}
-
-
-export default function DSR() {
-	markdown = convertToMarkdown("dsr.md");
-
-	return (
-		<p>Placeholder: Put dsr resources here</p>
-	);
-}
-
-
-export default function TOP() {
-	markdown = convertToMarkdown("top.md");
-
-	return (
-		<p>Placeholder: Put top resources here</p>
-	);
-}
+// export default function UWU() {
+	// markdown = convertToMarkdown("uwu.md");
+// 
+	// return (
+		// <p>Placeholder: Put uwu resources here</p>
+	// );
+// }
+// 
+// 
+// export default function UCOB() {
+	// markdown = convertToMarkdown("ucob.md");
+// 
+	// return (
+		// <p>Placeholder: Put ucob resources here</p>
+	// );
+// }
+// 
+// 
+// export default function TEA() {
+	// markdown = convertToMarkdown("tea.md");
+// 
+	// return (
+		// <p>Placeholder: Put tea resources here</p>
+	// );
+// }
+// 
+// 
+// export default function DSR() {
+	// markdown = convertToMarkdown("dsr.md");
+// 
+	// return (
+		// <p>Placeholder: Put dsr resources here</p>
+	// );
+// }
+// 
+// 
+// export default function TOP() {
+	// markdown = convertToMarkdown("top.md");
+// 
+	// return (
+		// <p>Placeholder: Put top resources here</p>
+	// );
+// }
 
 
 // export default function FRU() {
