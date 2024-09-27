363469f2e71c36e16926ac956efca654dbed9aa7
Mera <krasnovian7.pub@gmail.com>
2024-09-27 11:18:09 -0700

    dsr converted to jsx

--- naurffxiv/src/app/page.js
+++ naurffxiv/src/app/page.js
@@ -2,12 +2,14 @@ import react from "react";
 import { Container } from "@mui/material";
 import { UWU } from "./ultimates/uwu.js";
 import {UCOB} from "./ultimates/ucob.js";
+import { TEA } from "./ultimates/tea.js";
+import { DSR } from "./ultimates/dsr.js";
 
 
 export default function Home() {
   return (
 	<Container sx={{ height: "100vh"}}>
-		<UCOB />
+		<DSR />
 	</Container>
   );
 }
