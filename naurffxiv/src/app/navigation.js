339e722ecaaddc8967669e3a86a7ecf27e62e62e
Brian Tran <brtran@umich.edu>
2024-10-05 21:37:47 -0400

    Span navigation bar across entire page

--- naurffxiv/src/app/navigation.js
+++ naurffxiv/src/app/navigation.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
@@ -8,44 +8,39 @@ import IconButton from '@mui/material/IconButton';
 // import MenuIcon from '@mui/icons-material/Menu';
 
 export default function NavBar() {
-	return (
-        <>
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="small"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 0 }}
-            >
-            </IconButton>
-            <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
-              badussy
-              <a href = "https://www.w3schools.com/tags/tag_a.asp" >
-                Dawntrail
-              </a>
-            </Typography>
-            <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
-            <a href = "https://www.w3schools.com/tags/tag_a.asp">
-                Endwalker
-              </a>
-            </Typography>
-            <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
-            <a href = "https://www.w3schools.com/tags/tag_a.asp">
-                Shadowbringers
-              </a>
-            </Typography>
-            <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
-            <a href = "https://www.w3schools.com/tags/tag_a.asp">
-                Stormblood
-              </a>
-            </Typography>
-            <Button color="inherit">Badussy</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-        </>
-    );
+  return (
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="small"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 0 }}
+          />
+        <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
+          badussy
+          <a href = "https://www.w3schools.com/tags/tag_a.asp" >
+            Dawntrail
+          </a>
+        </Typography>
+        <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
+        <a href = "https://www.w3schools.com/tags/tag_a.asp">
+            Endwalker
+          </a>
+        </Typography>
+        <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
+        <a href = "https://www.w3schools.com/tags/tag_a.asp">
+            Shadowbringers
+          </a>
+        </Typography>
+        <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
+        <a href = "https://www.w3schools.com/tags/tag_a.asp">
+            Stormblood
+          </a>
+        </Typography>
+        <Button color="inherit">Badussy</Button>
+      </Toolbar>
+    </AppBar>
+  );
 }
--- naurffxiv/src/app/page.js
+++ naurffxiv/src/app/page.js
@@ -4,16 +4,17 @@ import NavBar from "./navigation.js";
 import UWU from "./ultimates.js";
 
 
+
+
 export default function Home() {
   return (
-	<>
-	<Container sx={{ maxHeight: "10vh"}}>
-		<NavBar />
-	</Container>
-	<Container sx={{ maxWidth: "10vh"}}>
-		<UWU />
-	</Container>
-	</>
+    <>
+      <Container disableGutters maxWidth={false}>
+        <NavBar />
+      </Container>
+      <Container sx={{ maxWidth: "10vh"}}>
+        <UWU />
+      </Container>
+    </>
   );
 }
-
