4ccf16e256814d492a0229eaa43a8cbdbbaf7480
GitButler <gitbutler@gitbutler.com>
2024-10-07 19:49:28 -0700

    GitButler WIP Commit

--- naurffxiv/src/app/navigation.js
+++ naurffxiv/src/app/navigation.js
@@ -1,51 +1,58 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import { Container } from "@mui/material";
+import { Grid } from '@mui/material';
+import discordlogo from './img/discordlogo.png';
 // import MenuIcon from '@mui/icons-material/Menu';
 
 export default function NavBar() {
-	return (
-        <>
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="small"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 0 }}
-            >
-            </IconButton>
-            <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
-              badussy
-              <a href = "https://www.w3schools.com/tags/tag_a.asp" >
-                Dawntrail
-              </a>
-            </Typography>
-            <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
-            <a href = "https://www.w3schools.com/tags/tag_a.asp">
-                Endwalker
-              </a>
-            </Typography>
-            <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
-            <a href = "https://www.w3schools.com/tags/tag_a.asp">
-                Shadowbringers
-              </a>
-            </Typography>
-            <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
-            <a href = "https://www.w3schools.com/tags/tag_a.asp">
-                Stormblood
-              </a>
-            </Typography>
-            <Button color="inherit">Badussy</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-        </>
-    );
+
+  return (
+    <Container disableGutters maxWidth={false}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="small"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 0 }}
+          />
+          <Grid container >
+            <Grid item xs={6} style={{ textAlign: 'left' }} sx={{ display: 'inline-flex' }}>
+              <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
+                badussy
+                <a href = "https://www.w3schools.com/tags/tag_a.asp" >
+                  Dawntrail
+                </a>
+              </Typography>
+              <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
+              <a href = "https://www.w3schools.com/tags/tag_a.asp">
+                  Endwalker
+                </a>
+              </Typography>
+              <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
+              <a href = "https://www.w3schools.com/tags/tag_a.asp">
+                  Shadowbringers
+                </a>
+              </Typography>
+              <Typography justify-content="" variant="h8" component="div" sx={{ flexGrow: 1 }}>
+              <a href = "https://www.w3schools.com/tags/tag_a.asp">
+                  Stormblood
+                </a>
+              </Typography>
+            </Grid>
+          </Grid>
+          <a href = "https://discord.gg/naurffxiv" target = "_blank" rel="noopener noreferrer">
+        <img src={discordlogo} alt="Discord Logo" ></img>
+        </a>
+      </Toolbar>
+    </AppBar>
+    </Container>
+  );
 }
+	
--- naurffxiv/src/app/page.js
+++ naurffxiv/src/app/page.js
@@ -4,16 +4,15 @@ import NavBar from "./navigation.js";
 import UWU from "./ultimates.js";
 
 
+
+
 export default function Home() {
   return (
-	<>
-	<Container sx={{ maxHeight: "10vh"}}>
-		<NavBar />
-	</Container>
-	<Container sx={{ maxWidth: "10vh"}}>
-		<UWU />
-	</Container>
-	</>
+    <>
+        <NavBar />
+      <Container sx={{ maxWidth: "10vh"}}>
+        <UWU />
+      </Container>
+    </>
   );
 }
-
