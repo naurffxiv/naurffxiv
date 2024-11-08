1e27fb0e637262adf4998f27b55f57e753944b62
OTCompa <41562938+OTCompa@users.noreply.github.com>
2024-11-07 21:44:28 -0600

    update icon and page titles

--- naurffxiv/theme.config.jsx
+++ naurffxiv/theme.config.jsx
@@ -1,5 +1,24 @@
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 export default {
+    head: function useHead() {
+      const { asPath } = useRouter()
+      const config = useConfig()
+      const { route } = useRouter()
+      const title = config.title + (route === '/' ? '' : ' - NAUR')
+      return (
+        <>
+        <title>{title}</title>
+        <meta property="og:title" content={title} />
+        <link
+          rel="icon"
+          href="/naurfavicon.png"
+          type="image/png"
+        />
+        </>
+      )
+    },
     logo: <><img width="48" src='/images/naur.png' /></>,
     docsRepositoryBase: "https://github.com/naurffxiv/naurffxiv",
     project: {
