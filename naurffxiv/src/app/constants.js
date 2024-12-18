651e3a5f8afb84c359875b2e035c262587a6630d
Brian Tran <44913669+brtran4@users.noreply.github.com>
2024-12-17 20:30:40 -0500

    Implement Navigation Bar (#34)
    This commit lays out the fundamentals for what we need from the
    Navigation Bar. A later commit will be pushed out when specific theming
    is set.
    
    This is what the navigation bar currently looks like
    
    <img width="1199" alt="Screenshot 2024-11-14 at 1 36 10 PM"
    src="https://github.com/user-attachments/assets/1c4ec891-2b9a-475d-9217-3b7560e3073e">
    
    Ticket: NAUR-23

---
+++ naurffxiv/src/app/constants.js
@@ -0,0 +1,44 @@
+export const pages = [
+  {
+    "name": "Events",
+    "link": "/events",
+    "login_required": false,
+  },
+  {
+    "name": "Contact Us",
+    "link": "/contact-us",
+    "login_required": false,
+  },
+  {
+    "name": "Staff",
+    "link": "/staff",
+    "login_required": true,
+  },
+]
+
+export const ultimateList = [
+  {
+    "name": "Futures Rewritten",
+    "link": "/ultimates/fru",
+  },
+  {
+    "name": "The Omega Protocol",
+    "link": "/ultimates/top",
+  },
+  {
+    "name": "Dragonsong's Reprise",
+    "link": "/ultimates/dsr",
+  },
+  {
+    "name": "The Epic of Alexander",
+    "link": "/ultimates/tea",
+  },
+  {
+    "name": "The Weapon's Refrain",
+    "link": "/ultimates/uwu",
+  },
+  {
+    "name": "The Unending Coil of Bahamut",
+    "link": "/ultimates/ucob",
+  },
+]
