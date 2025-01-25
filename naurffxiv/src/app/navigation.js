33b92564b7d3c9c49114ef2ae37ab92cb471acf8
oytundd <oytundd@gmail.com>
2025-01-25 05:08:17 +0300

    fixed a bug with navbar not staying same color between themes

--- naurffxiv/src/app/navigation.js
+++ naurffxiv/src/app/navigation.js
@@ -26,7 +26,7 @@ export default function NavBar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed" sx={{ bgcolor: "#28506e", "& .MuiTypography-root": {color: "#ffffff"} }}>
+      <AppBar position="fixed" elevation={0} sx={{ bgcolor: "#28506e", "& .MuiTypography-root": {color: "#ffffff"} }}>
         <Toolbar sx={{ px: 2 }} disableGutters>
           <Typography sx={{ flexGrow: 1}}>
             <IconButton size="small">
