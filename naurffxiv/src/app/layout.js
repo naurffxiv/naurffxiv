4a49a7851710f73f843f572df1d527b86635c7e5
oytundd <oytundd@gmail.com>
2025-01-25 04:19:16 +0300

    dark mode toggle

--- naurffxiv/src/app/layout.js
+++ naurffxiv/src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+import { ThemeContextProvider } from './themeContext';
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ThemeContextProvider>
+        {children}
+      </ThemeContextProvider>
+      </body>
     </html>
   );
 }
---
+++ naurffxiv/src/app/themeContext.js
@@ -0,0 +1,36 @@
+// ThemeContext.js
+'use client';
+import React, { createContext, useState, useContext } from 'react';
+import { ThemeProvider } from '@mui/material';
+import { lightTheme, darkTheme } from './theme';
+
+const ThemeContext = createContext({
+  isDarkMode: false,
+  toggleTheme: () => {},
+});
+
+export function useThemeMode() {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useThemeMode must be used within ThemeContextProvider');
+  }
+  return context;
+}
+
+export function ThemeContextProvider({ children }) {
+	const [isDarkMode, setIsDarkMode] = useState(true);
+	const theme = isDarkMode ? darkTheme : lightTheme;
+  
+	const toggleTheme = () => {
+		console.log("Theme toggled, isDarkMode:", !isDarkMode);
+		setIsDarkMode(!isDarkMode);
+	};
+  
+	return (
+	  <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+		<ThemeProvider theme={theme}>
+		  {children}
+		</ThemeProvider>
+	  </ThemeContext.Provider>
+	);
+  }
