e8a5885197a8659094dcf268c27f96a0e40ef983
oytundd <oytundd@gmail.com>
2025-02-03 07:24:51 +0300

    responsive nav bar
    added a hamburger menu that appears on mobile(xs) screen and hidden on desktop screen
    dropdown items are right aligned in hamburger menu

--- naurffxiv/src/app/dropdown.js
+++ naurffxiv/src/app/dropdown.js
@@ -8,48 +8,88 @@ import {
   Button,
   IconButton,
   Menu,
-  MenuItem
+  MenuItem,
+  Box
 } from "@mui/material";
-
 import Link from 'next/link';
 import { ultimateList } from './constants.js';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 export default function UltimateDropdown(props) {
-  const {name} = props;
+  const {name, isMobile} = props;
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
+  
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
+  
   const handleClose = () => {
     setAnchorEl(null);
   };
 
+  // Menu props based on mobile/desktop mode
+  const menuProps = {
+    id: "basic-menu",
+    anchorEl: anchorEl,
+    open: open,
+    onClose: handleClose,
+    MenuListProps: {'aria-labelledby': 'basic-button'},
+    ...(isMobile && {
+      anchorOrigin: {
+        vertical: 'bottom',
+        horizontal: 'right',
+      },
+      transformOrigin: {
+        vertical: 'top',
+        horizontal: 'right',
+      },
+      sx: {
+        '& .MuiMenu-paper': {
+          width: 200,
+        },
+        '& .MuiMenuItem-root': {
+          justifyContent: 'flex-end',
+          padding: '8px 16px',
+          '& a': {
+            width: '100%',
+            textAlign: 'right',
+            whiteSpace: 'normal',
+            wordBreak: 'break-word',
+            lineHeight: '1.4',
+            display: 'block',
+            paddingLeft: '8px' 
+          }
+        }
+      }
+    })
+  };
+
   return (
-    <>
-      <Button
-        style={{color: 'white', textTransform: 'none'}}
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-      >
-        <Typography variant="h6" sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' }} }>
-          <Typography sx={{ textAlign:'center' }}>
-            {name}
-          </Typography>
+    <>      
+      {isMobile ? (
+        <Typography onClick={handleClick} sx={{ width: '100%', textAlign: 'right' }}>
+          {name}
           <ArrowDropDownIcon/>
         </Typography>
-      </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{'aria-labelledby': 'basic-button',}}
-      >
+      ) : (
+        <Button
+          style={{color: 'white', textTransform: 'none'}}
+          id="basic-button"
+          aria-controls={open ? 'basic-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+        >
+          <Typography variant="h6" sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' }} }>
+            <Typography sx={{ textAlign:'center' }}>
+              {name}
+            </Typography>
+            <ArrowDropDownIcon/>
+          </Typography>
+        </Button>
+      )}
+      <Menu {...menuProps}>
         {ultimateList.map((fight, i) => (
           <li key={i}>
             <MenuItem onClick={handleClose}>
@@ -61,5 +101,5 @@ export default function UltimateDropdown(props) {
         ))}
       </Menu>
     </>
-  )
+  );
 }
