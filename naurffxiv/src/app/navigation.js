e28176d066338a6d68a95093eacfe8cf384978ca
oytundd <oytundd@gmail.com>
2025-02-04 05:43:58 +0300

    merge bug fix
    fixed dark theme toggle changing the colors of dropdown menu unintentionally
    fixed dark theme toggle changing the color of navbar menu unintentionally
    fixed dark theme toggle changing the color of hamburger menu uintentionally

--- naurffxiv/src/app/navigation.js
+++ naurffxiv/src/app/navigation.js
@@ -33,7 +33,7 @@ export default function NavBar() {
   const { isDarkMode, toggleTheme } = useThemeMode();
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed" color="primary">
+      <AppBar position="fixed" color="primary" elevation={0}>
         <Toolbar sx={{ px: 2, width: '100%', maxWidth: '100vw' }} disableGutters>
           <Typography sx={{ flexGrow: 1}}>
             <IconButton size="small">
