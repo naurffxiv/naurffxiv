177100067e1a74ffaa9a21ab370b2c68721bc8b4
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-02-27 20:58:24 -0600

    change header and fix scaling

--- naurffxiv/src/components/Homepage/Header.js
+++ naurffxiv/src/components/Homepage/Header.js
@@ -1,20 +1,16 @@
 
 import { Box } from '@mui/material';
 import Image from 'next/image';
+import TitleImage from '/public/images/title-server-header.png';
 
 export default function Carousel({className}) {
     return (
         <Box>
-            <Box sx={{
-                width: 'auto',
-                height: '500px',
-                overflow: 'hidden',
-                position: 'relative'
-                }}
-                className={{className}}
-                >
-                <Image fill src='/images/fru-server-header.png' alt='banner' />
-            </Box>
+          <Box className='relative h-[25em] mt-16 grid justify-center content-center' sx={{backgroundImage: 'url(/images/fru-server-header.png)', backgroundSize: "cover", backgroundAttachment: "auto", boxShadow: '0 6px 6px -1px rgb(0 0 0 / 0.3)', backgroundPosition: 'right 50% top', backgroundRepeat: 'no-repeat'}}>
+
+              <Image src={TitleImage} style={{width: 'auto', height: 'auto', maxHeight: '10em'}} alt='banner' className="object-contain"/>
+
+          </Box>
         </Box>
     )
 }
