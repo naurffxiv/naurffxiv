83ea51acd29c3e034180cb3705bd1a1b2e931c62
Claire <nicholas.mellinger@outlook.com>
2025-03-06 16:25:32 -0500

    create Table of Contents (#65)
    Ticket: NAUR-22
    - TOC made with remark-toc
    - Heading ids made with rehype-slug
    - Heading links made with rehype-autolink-headings

--- naurffxiv/next.config.mjs
+++ naurffxiv/next.config.mjs
@@ -2,12 +2,29 @@
 import createMDX from '@next/mdx';
 import remarkFrontmatter from 'remark-frontmatter';
 import rehypeImgSize from 'rehype-img-size';
+import remarkToc from "remark-toc";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [remarkFrontmatter],
-    rehypePlugins: [[rehypeImgSize, { dir: 'public' }]],
+    remarkPlugins: [remarkFrontmatter, remarkToc],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeImgSize, { dir: 'public' }],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behaviour: 'append',
+          properties: {
+            ariaHidden: true,
+            tabIndex: -1,
+            className: 'hash-link',
+          },
+        },
+      ],
+    ],
   },
 });
 
