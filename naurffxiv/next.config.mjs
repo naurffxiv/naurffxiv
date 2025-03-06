fcad1e8c9e211db31142b06ee8b9253f3d474af8
Claire <nicholas.mellinger@outlook.com>
2025-03-06 09:36:07 -0500

    create Table of Contents
    Ticket: NAUR-22
    - TOC made with remark-toc
    - Heading ids made with rehype-slug
    - Heading links made with rehype-autolink-headings

--- naurffxiv/next.config.mjs
+++ naurffxiv/next.config.mjs
@@ -2,18 +2,36 @@
 import createMDX from '@next/mdx';
 import remarkFrontmatter from 'remark-frontmatter';
 import rehypeImgSize from 'rehype-img-size';
+import remarkToc from "remark-toc";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [remarkFrontmatter],
-    rehypePlugins: [[rehypeImgSize, { dir: 'public' }]],
+    remarkPlugins: [remarkFrontmatter, remarkToc],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeImgSize, { dir: 'public' }],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behaviour: 'append',
+          properties: {
+            ariaHidden: true,
+            tabIndex: -1,
+            className: 'hash-link',
+          },
+        },
+      ],
+    ],
   },
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  reactStrictMode: true,
 };
 
 export default withMDX(nextConfig);
