8cae1c86e1fd304c93aa9d8bc4a36fe13fc83b85
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-03-08 01:07:14 -0600

    cleanup

--- naurffxiv/src/app/layout.js
+++ naurffxiv/src/app/layout.js
@@ -4,6 +4,10 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 import { ThemeContextProvider } from './themeContext';
 
+export const metadata = {
+  title: "NAUR",
+  description: "NA Ultimate Raiding - FFXIV",
+};
 
 export default function RootLayout({ children }) {
   return (
