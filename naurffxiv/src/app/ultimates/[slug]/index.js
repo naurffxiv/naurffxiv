4675e6f2e90ccefac2a45e317c35d4e4e6214731
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-03-08 01:08:19 -0600

    separate toc into its own component

--- naurffxiv/src/app/ultimates/[slug]/index.js
+++ naurffxiv/src/app/ultimates/[slug]/index.js
@@ -1,32 +1,14 @@
 "use client";
 import MdxLayout from "@/components/Mdx/MdxLayout"
 import React from "react";
-function recursiveToc(toc) {
-    if (!toc) {
-        return
-    }
-    const currLevel = toc.map((li) => (
-          <li key={li.id}>
-            <a href={`#${li.id}`} className="toc-links">
-                {li.value}
-            </a>
-            {recursiveToc(li.children)}
-          </li>
-    ))
-    return (
-        <ul className="list-none ps-1 toc-links">
-            {currLevel}
-        </ul>
-    )
-}
+import TableOfContents from "@/components/Mdx/TableOfContents";
 
 const MDXPage = ({params: content, toc}) => {
     return (
         <MdxLayout>
-            <div className="grid grid-cols-1 md:grid-cols-[1fr_2fr_1fr] max-w-[100rem] mx-auto py-6 text-blue-300">
-                    
-                <div className="prose prose-invert top-[5.5rem] self-start hidden lg:block sticky toc-links">
-                {recursiveToc(toc)}
+            <div className="grid grid-cols-1 md:grid-cols-[1fr_2fr_1fr] max-w-[100rem] mx-auto py-6">
+                <div className="prose prose-invert top-[5.5rem] self-start hidden lg:block sticky">
+                    <TableOfContents toc={toc} />
                 </div>
 
                 <article className="max-w-[100ch] prose prose-invert m-auto mx-6">
