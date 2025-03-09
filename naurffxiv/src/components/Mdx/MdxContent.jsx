8cd5434ee3d5cfdbda43128e1ce02b2ae65ad605
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-03-08 20:07:04 -0600

    requested changes

--- naurffxiv/src/components/Mdx/MdxContent.jsx
+++ naurffxiv/src/components/Mdx/MdxContent.jsx
@@ -3,11 +3,9 @@ import MdxLayout from "./MdxLayout"
 export default function MdxContent({ children }) {
   return (
     <MdxLayout>
-        <div>
-            <div className="max-w-[100ch] m-auto prose prose-invert">
-                {children}
-            </div>
-        </div>
+      <div className="max-w-[100ch] m-auto prose prose-invert">
+          {children}
+      </div>
     </MdxLayout>
   );
 }
