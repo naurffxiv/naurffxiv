6633c39b520ac525363d15385afad03ae529694d
Claire <nicholas.mellinger@outlook.com>
2025-03-09 16:14:35 -0400

    roboto fix

--- naurffxiv/src/app/layout.js
+++ naurffxiv/src/app/layout.js
@@ -1,7 +1,10 @@
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto  = Roboto({ 
+  weight: ['300','400','500','700'],
+  style: ['normal','italic'],
+  subsets: ["latin"] });
 
 export const metadata = {
   title: "NAUR",
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
           {children}
       </body>
     </html>
