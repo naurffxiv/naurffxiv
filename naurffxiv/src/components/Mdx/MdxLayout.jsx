205584ce3c3432a095c922df19355968c1460708
Claire <nicholas.mellinger@outlook.com>
2025-03-08 21:36:28 -0500

    Change global font to Roboto (#73)
    Ticket: NAUR-60
    - Change global font to robot
    - Remove unused images
    
    ---------
    
    Co-authored-by: OTCompa <41562938+OTCompa@users.noreply.github.com>

--- naurffxiv/src/components/Mdx/MdxLayout.jsx
+++ naurffxiv/src/components/Mdx/MdxLayout.jsx
@@ -2,13 +2,23 @@ import "@/app/globals.css";
 import { ThemeContextProvider } from '@/app/themeContext';
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ['300','400','500','700'],
+  style: ['normal','italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function MdxLayout({ children }) {
   return (
+    <div className={roboto.className}>
       <ThemeContextProvider>
         <NavBar />
         {children}
         <Footer />
       </ThemeContextProvider>
+    </div>
   );
 }
