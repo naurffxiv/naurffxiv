297479122fe5d38cc0c55cee154a0d5848b0a374
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-03-12 01:13:46 -0500

    move navbar and footer to layout.js

--- naurffxiv/src/app/layout.js
+++ naurffxiv/src/app/layout.js
@@ -1,6 +1,9 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
 
+import NavBar from '@/components/NavBar/NavBar';
+import Footer from '@/components/Footer/Footer';
+
 const roboto = Roboto({
   weight: ['300','400','500','700'],
   style: ['normal','italic'],
@@ -15,7 +18,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-          {children}
+          <NavBar />
+          <main>
+            {children}
+          </main>
+          <Footer />
       </body>
     </html>
   );
--- naurffxiv/src/app/page.js
+++ naurffxiv/src/app/page.js
@@ -2,11 +2,9 @@
 import React from 'react';
 import { Box } from '@mui/material';
 
-import NavBar from '@/components/NavBar/NavBar';
 import Carousel from '@/components/Homepage/Carousel';
 import HomepageContent from '@/components/Homepage/HomepageContent';
 import Header from '@/components/Homepage/Header';
-import Footer from '@/components/Footer/Footer';
 
 export default function Home() {
   return (
@@ -14,14 +12,11 @@ export default function Home() {
       <Box sx={{ 
         minHeight: '100vh',
         width: '100%',
-        paddingBottom: '5rem',
       }}>
-        <NavBar />
         <Header />
         <Carousel />
         <HomepageContent />
       </Box>
-      <Footer />
     </Box>
   );
 }
