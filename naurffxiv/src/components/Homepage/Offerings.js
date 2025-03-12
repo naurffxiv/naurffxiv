eee23c6ef557fb2f692a4f3a1f8c27fce0290f41
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-03-12 01:20:32 -0500

    homepage offerings redesign

---
+++ naurffxiv/src/components/Homepage/Offerings.js
@@ -0,0 +1,30 @@
+import { icons } from '@/app/constants.js';
+import Image from 'next/image';
+
+const divs = [
+    { icon: icons.chat, width: 70, height: 70, alt: "Chat icon", text: "Active Staff", desc: "Active staff and discussion channels" },
+    { icon: icons.lightbulb, width: 40, height: 40, alt: "Bulb icon", text: "Strats & Resources", desc: "Party Finder-friendly strats and resources" },
+    { icon: icons.contact, width: 80, height: 80, alt: "Contact icon", text: "Recruitment Forums", desc: "Recruitment forums for statics and Party Finder" },
+    { icon: icons.discord, width: 50, height: 50, alt: "Discord icon", text: "Discord Bots", desc: "Display Party Finder listings and raiding achievements" },
+    { icon: icons.calendar, width: 45, height: 45, alt: "Calendar icon", text: "Server Events", desc: "Server events to help players prog and clear" }
+];
+
+export default function Offerings() {
+    return (
+        <div>
+            <h3 className="text-xl">What we offer</h3>
+            <div className="mt-3 flex gap-x-10 gap-y-3 md:gap-y-6 justify-center text-center mx-auto flex-row flex-wrap">
+                {
+                    divs.map(div => {
+                        return (
+                            <div key={div.alt} className="bg-[#007EA7] grid grid-rows-2 rounded-xl pt-10 pb-6 px-3 min-w-[10rem] max-w-[17rem] flex-1">
+                                <Image src={div.icon} width={div.width} height={div.height} alt={div.alt} className="m-auto"/>
+                                <p className="text-center mt-2">{div.desc}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
