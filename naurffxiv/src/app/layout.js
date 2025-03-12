19a3fc09892e3247e444259277d2199e6fdc44b5
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-03-12 18:24:27 -0500

    requested changes

--- naurffxiv/src/app/layout.js
+++ naurffxiv/src/app/layout.js
@@ -19,9 +19,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-          <MUITheme>
-            {children}
-          </MUITheme>
+        <MUITheme>
+          {children}
+        </MUITheme>
       </body>
     </html>
   );
--- naurffxiv/src/components/Mdx/MdxLayout.jsx
+++ naurffxiv/src/components/Mdx/MdxLayout.jsx
@@ -4,7 +4,7 @@ import Footer from "../Footer/Footer";
 import { Roboto } from "next/font/google";
 import MUITheme from "@/app/theme";
 
-// note: this can be removed when MDX rendering is moved to @/app/
+// note: this can be removed when MDX rendering is moved to @/app/ (NAUR-84)
 const roboto = Roboto({
   weight: ['300','400','500','700'],
   style: ['normal','italic'],
