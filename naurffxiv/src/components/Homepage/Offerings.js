30e06c97510dd7720cea0412db5450d8831a7a87
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-03-13 14:24:54 -0500

    adjust offerings

--- naurffxiv/src/components/Homepage/Offerings.js
+++ naurffxiv/src/components/Homepage/Offerings.js
@@ -1,8 +1,6 @@
 import Image from 'next/image';
 import { icons } from '@/app/assets'
 
-
-
 const divs = [
     { icon: icons.Chat, width: 70, height: 70, alt: "Chat icon", text: "Active Staff", desc: "Active staff and discussion channels" },
     { icon: icons.Lightbulb, width: 40, height: 40, alt: "Bulb icon", text: "Strats & Resources", desc: "Party Finder-friendly strats and resources" },
@@ -15,13 +13,13 @@ export default function Offerings() {
     return (
         <div>
             <h3 className="text-xl">What we offer</h3>
-            <div className="mt-3 flex gap-x-10 gap-y-3 md:gap-y-6 justify-center text-center mx-auto flex-row flex-wrap">
+            <div className="mt-3 flex gap-x-3 sm:gap-x-10 gap-y-3 md:gap-y-6 justify-center text-center mx-auto flex-row flex-wrap">
                 {
                     divs.map(div => {
                         return (
-                            <div key={div.alt} className="bg-[#007EA7] grid grid-rows-2 rounded-xl pt-10 pb-6 px-3 min-w-[10rem] max-w-[17rem] flex-1">
+                            <div key={div.alt} className="bg-[#007EA7] grid grid-rows-2 rounded-xl min-w-28 max-w-36 px-1 py-3 sm:pt-10 sm:pb-6 sm:px-3 sm:min-w-48 sm:max-w-48 flex-1">
                                 <Image src={div.icon} width={div.width} height={div.height} alt={div.alt} className="m-auto"/>
-                                <p className="text-center mt-2">{div.desc}</p>
+                                <p className="text-center sm:mt-2">{div.desc}</p>
                             </div>
                         )
                     })
