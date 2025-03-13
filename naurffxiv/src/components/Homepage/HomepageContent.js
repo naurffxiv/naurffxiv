b215dac6b782ab6c5b0acdb0e3dae6a2943623ae
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-03-13 14:11:15 -0500

    move info cards below fight guide list

--- naurffxiv/src/components/Homepage/HomepageContent.js
+++ naurffxiv/src/components/Homepage/HomepageContent.js
@@ -1,13 +1,11 @@
-import { icons } from '@/app/constants.js';
-import Image from 'next/image';
 import FightGuideList from './FightGuideList';
 import Offerings from './Offerings';
 
 export default function HomepageContent() {
     return (
         <div className="max-w-screen-2xl mx-auto p-10 grid gap-y-10">
-            <Offerings />
             <FightGuideList />
+            <Offerings />
             <p className="text-center text-xl">
                 If you are interested in Ultimate Raiding on NA Data Centers and what we have to offer, come be a part of our community!
             </p>
