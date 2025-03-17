c7a8da9913a4c07093791d5ab75942e125da98bc
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-03-17 03:13:42 -0500

    implement twitch clip component

--- naurffxiv/src/components/Mdx/MdxComponents.js
+++ naurffxiv/src/components/Mdx/MdxComponents.js
@@ -1,4 +1,5 @@
 import YouTube from "../Video/YouTube";
+import TwitchClip from "../Video/TwitchClip";
 import { CopyToClipboard } from '@/components/Mdx/CopyToClipboard';
 import TwitchVoD from "../Video/TwitchVoD";
 
@@ -8,5 +9,6 @@ export const MDXComponents = {
     h3: (props) => <section><h3 className="scroll-mt-20" {...props} /></section>,
     pre: (props) => <CopyToClipboard><pre {...props}></pre></CopyToClipboard>,
     YouTube,
+    TwitchClip,
     TwitchVoD,
 }
