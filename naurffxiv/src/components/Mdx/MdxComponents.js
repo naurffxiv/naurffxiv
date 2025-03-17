f3633b33e9ee9959c178ea42bf2166b0c2ab049a
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-03-17 12:20:19 -0500

    add streamable component

--- naurffxiv/src/components/Mdx/MdxComponents.js
+++ naurffxiv/src/components/Mdx/MdxComponents.js
@@ -2,6 +2,7 @@ import YouTube from "../Video/YouTube";
 import TwitchClip from "../Video/TwitchClip";
 import { CopyToClipboard } from '@/components/Mdx/CopyToClipboard';
 import TwitchVoD from "../Video/TwitchVoD";
+import Streamable from "../Video/Streamable";
 
 export const MDXComponents = { 
     h1: (props) => <h1 className="scroll-mt-20" {...props} />,
@@ -11,4 +12,5 @@ export const MDXComponents = {
     YouTube,
     TwitchClip,
     TwitchVoD,
+    Streamable,
 }
