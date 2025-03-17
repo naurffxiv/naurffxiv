c7842e4dea93a344c90f4664710d76205597a76f
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-03-17 01:25:10 -0500

    implement youtube component

--- naurffxiv/src/components/Mdx/MdxComponents.js
+++ naurffxiv/src/components/Mdx/MdxComponents.js
@@ -1,3 +1,4 @@
+import YouTube from "../Video/YouTube";
 import { CopyToClipboard } from '@/components/Mdx/CopyToClipboard';
 
 export const MDXComponents = { 
@@ -5,4 +6,5 @@ export const MDXComponents = {
     h2: (props) => <section><h2 className="scroll-mt-20" {...props} /></section>,
     h3: (props) => <section><h3 className="scroll-mt-20" {...props} /></section>,
     pre: (props) => <CopyToClipboard><pre {...props}></pre></CopyToClipboard>,
+    YouTube,
 }
