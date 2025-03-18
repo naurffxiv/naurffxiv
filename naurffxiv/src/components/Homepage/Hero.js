dc466c96fdfbe3077b3d58a11f8e4dbba7418ea0
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-03-18 18:55:09 -0500

    Set max width for hero and adjust style for wider resolutions (#100)
    Ticket: NAUR-105
    
    ![image](https://github.com/user-attachments/assets/44f422e8-7869-46c6-9ff5-8d27170e79a1)
    
    ![image](https://github.com/user-attachments/assets/7b6a54ea-3a51-403b-9efb-ad395d04c4f1)
    
    ![image](https://github.com/user-attachments/assets/a7cbddb7-b0f2-4cb8-9425-8e10fd910e7e)
    
    ![image](https://github.com/user-attachments/assets/dcf15be7-b7b5-44e7-81ce-4e9a9484b68a)
    
    ![image](https://github.com/user-attachments/assets/9d72c0dc-d722-4519-a5a7-4ae43883812e)

--- naurffxiv/src/components/Homepage/Hero.js
+++ naurffxiv/src/components/Homepage/Hero.js
@@ -2,28 +2,38 @@ import Image from "next/image"
 import { icons } from "@/app/assets"
 
 export default function Header() {
-    return (
-      <div className="grid min-w-full md:grid-cols-[40%_60%] xl:grid-cols-[45%_55%]">
-        <div className="px-5 xl:pr-0 bg-center bg-header-default md:bg-header-gradient md:bg-left">
-          <div className="grid mx-auto py-16 gap-y-3 max-w-prose md:gap-y-6 xl:float-right">
-            <h1 className="text-4xl xl:text-6xl font-bold">NAUR</h1>
-            <h3 className="text-lg/8 xl:text-2xl/10">
-              NA Ultimate Raiding (NAUR) is an FFXIV Discord community focused on High-End raiding on NA Data Centers. If you're looking to raid on NA Data Centers, come join our community!
-            </h3>
-            <a
-            href="https://discord.com/invite/naurffxiv"
-            target="_blank"
-            className="transition-colors bg-[#007EA7] rounded-full max-w-fit py-4 px-8 hover:bg-[#007196]"
-            >
-            <span className="flex gap-5 font-medium">
-              <Image src={icons.Discord} alt="Discord" width="32" height="24" />
-              Join our Discord!
-            </span>
-            </a>
+  function Text(){ return (
+      <div className="grid max-w-prose gap-y-3 md:gap-y-6">
+        <h1 className="text-4xl xl:text-6xl font-bold">NAUR</h1>
+                <h3 className="text-lg/8 xl:text-2xl/10">
+                  NA Ultimate Raiding (NAUR) is an FFXIV Discord community focused on High-End raiding on NA Data Centers. If you're looking to raid on NA Data Centers, come join our community!
+                </h3>
+                <a
+                href="https://discord.com/invite/naurffxiv"
+                target="_blank"
+                className="transition-colors bg-[#007EA7] rounded-full max-w-fit py-4 px-8 hover:bg-[#007196]"
+                >
+                <span className="flex gap-5 font-medium">
+                  <Image src={icons.Discord} alt="Discord" width="32" height="24" />
+                  Join our Discord!
+                </span>
+        </a>
+      </div>
+    )
+  }
+
+  return (
+      <div className="max-w-screen-2xl grid md:grid-cols-2 bg-[#1A192B] 2xl:bg-transparent 2xl:bg-header-gradient mx-auto 2xl:px-6">
+        <div className="bg-center bg-header-default px-5 md:bg-none md:bg-left py-16">
+          <div className="max-w-3xl mx-auto mr-0">
+            <div className="max-w-fit mx-auto">
+              <Text />
+            </div>
           </div>
         </div>
-        <div className="hidden md:block bg-header-wide bg-cover bg-center h-60 md:h-auto">
-          <div className="min-h-full bg-header-wide-gradient">
+        <div className="max-w-screen-md">
+          <div className="hidden md:block bg-header-wide bg-cover bg-center h-full">
+            <div className="h-full bg-header-right-gradient 2xl:bg-header-right-ultrawide-gradient"/>
           </div>
         </div>
       </div>
