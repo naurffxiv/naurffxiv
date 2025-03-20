905eb78c9816b9a13a6ea55240a18146590f0167
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-03-19 19:25:05 -0500

    Adjust homepage's styles + content (#101)
    Ticket: NAUR-106
    Adjusts some colors for the homepage and offering content

--- naurffxiv/src/components/Homepage/OfferingCards.js
+++ naurffxiv/src/components/Homepage/OfferingCards.js
@@ -1,31 +1,53 @@
 import Image from 'next/image';
-import { icons, iconsMui } from '@/app/assets'
+import { icons } from '@/app/assets'
 
 const divs = [
-    { icon: icons.Contact, width: 70, height: 70, alt: "Person Search Icon", text: "Community", desc: "Inclusive & Helpful Community" },
-    { icon: icons.Chat, width: 70, height: 70, alt: "Chat icon", text: "Active Discord", desc: "Active Staff & Discussion Channels" },
-    { icon: icons.Lightbulb, width: 40, height: 40, alt: "Bulb icon", text: "Strats & Resources", desc: "NA Party Finder Resources" },
-    { iconMui: iconsMui.PersonSearchIcon, fontSize: "70px",alt: "Contact icon", text: "Recruitment Tools", desc: "Static & Party Finder Recruitment Tools" },
-    { icon: icons.Discord, width: 50, height: 50, alt: "Discord icon", text: "Discord Bots", desc: "Custom Discord Bots Made By NAUR" },
-    { icon: icons.Calendar, width: 45, height: 45, alt: "Calendar icon", text: "Server Events", desc: "Server Events For the FFXIV Community" }
-];
+    {
+        title: "Active & Inclusive Community",
+        desc: `NAUR is an inclusive community with active staff and discussion channels.
+                Our Discord features custom recruitment tools and bots created by our members,
+                making it easy to find groups, recruit for high-end content, and stay connected with fellow players.`,
+        icon: icons.Contact,
+        width: 70,
+        height: 70,
+        alt: "Person Search Icon",
+    },
+    {
+        title: "FFXIV Resources for NA Players",
+        desc: `Our website and Discord provide tailored resources for high-end content which include up-to-date
+                raid-plans/toolboxes, written guides, and other learning tools and resources.
+                Whether you're tackling Savage or Ultimate raids, we’ve got the tools you need to progress and clear them.`,
+        icon: icons.Lightbulb,
+        width: 40,
+        height: 40,
+        alt: "Bulb icon",
+    },
+    {
+        title: "Raiding Server Events",
+        desc: `NAUR hosts a variety of events throughout the year for NA data centers,
+                designed to help players prog and clear high-end content.
+                Join us for fun parties that bring the community together to achieve your raiding goals.`,
+        icon: icons.Calendar,
+        width: 45,
+        height: 45,
+        alt: "Calendar icon",
+    }
+]
 
 export default function Offerings() {
     return (
         <div className="flex justify-center items-center flex-col">
-            <h3 className="text-xl">What NAUR has to offer</h3>
-            <div className="mt-3 flex gap-x-3 sm:gap-x-10 gap-y-3 md:gap-y-6 justify-center text-center mx-auto flex-row flex-wrap">
+            <h3 className="text-2xl">What NAUR has to offer</h3>
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-3 sm:gap-x-10 gap-y-3 justify-center text-center mx-auto">
                 {
                     divs.map(div => {
                         return (
-                            <div key={div.alt} className="bg-[#007EA7] grid grid-rows-2 rounded-xl min-w-28 max-w-36 px-1 py-3 sm:pt-10 sm:pb-6 sm:px-3 sm:min-w-48 sm:max-w-48 flex-1">
-                                {
-                                    div.icon ? 
-                                    <Image src={div.icon} width={div.width} height={div.height} alt={div.alt} className="m-auto"/>
-                                    :
-                                    <div.iconMui sx={{fontSize: div.fontSize}} className="m-auto"/>
-                                }
-                                <p className="text-center sm:mt-2">{div.desc}</p>
+                            <div key={div.alt} className="grid grid-rows-[1fr_2fr] rounded-xl px-1 sm:pb-6 sm:px-3 flex-1">
+                                <Image src={div.icon} width={div.width} height={div.height} alt={div.alt} className="m-auto mb-0"/>
+                                <div>
+                                    <h4 className="text-lg font-semibold text-center sm:mt-2">{div.title}</h4>
+                                    <p className="max-w-prose prose prose-invert">{div.desc}</p>
+                                </div>
                             </div>
                         )
                     })
