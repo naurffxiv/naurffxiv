47134680b42a838f21c2593f2a80deb96d7a20b7
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-03-21 20:33:05 -0500

    Implement optional ToC collapsing (#114)
    Ticket: NAUR-120
    This PR implements ToC collapsing which can be toggled by setting
    `collapseToc` to true in the frontmatter of an mdx file:
    ```
    ---
    title: Futures Rewritten
    order: 0
    collapseToc: true
    ---
    ```
    
    ## Original
    
    ![image](https://github.com/user-attachments/assets/06164864-ab51-464b-822a-0b72ba4371cf)
    
    ## With collapsing ToC
    
    ![image](https://github.com/user-attachments/assets/89fa302f-5a42-4c7e-bb1e-42e39b20c0ad)
    
    ![image](https://github.com/user-attachments/assets/90256f6e-85c6-4f0a-9c8a-efbf6ff16ed1)
    
    Also want opinions on color selection of the text if y'all got any
    Note: Testing this with #115 on the same branch may be ideal

--- src/app/[difficulty]/[[...slug]]/index.js
+++ src/app/[difficulty]/[[...slug]]/index.js
@@ -3,13 +3,14 @@ import MdxLayout from "@/components/Mdx/MdxLayout"
 import React from "react";
 import TableOfContents from "@/components/Mdx/TableOfContents";
 import QuickLinks from "@/components/Mdx/QuickLinks";
+import "./mdx.css";
 
-const MDXPage = ({children, toc, metadata, slug}) => {
+const MDXPage = ({children, toc, siblingData, slug, frontmatter}) => {
     return (
         <MdxLayout>
             <div className="grid grid-cols-1 lg:grid-cols-[90ch_1fr] xl:grid-cols-[1fr_90ch_1fr] max-w-screen-2xl mx-auto py-6">
                 <div className="prose prose-invert top-[5.5rem] self-start hidden xl:block sticky">
-                    <TableOfContents toc={toc} />
+                    <TableOfContents toc={toc} frontmatter={frontmatter}/>
                 </div>
 
                 <article className="max-w-[90ch] prose prose-invert m-auto mx-6">
@@ -17,7 +18,7 @@ const MDXPage = ({children, toc, metadata, slug}) => {
                 </article>
 
                 <div className="prose prose-invert top-[5.5rem] self-start hidden lg:block sticky">
-                    <QuickLinks metadata={metadata} slug={slug}/>
+                    <QuickLinks siblingData={siblingData} slug={slug}/>
                 </div>
             </div>
         </MdxLayout>
