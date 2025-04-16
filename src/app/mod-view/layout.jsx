ec1f318cd04178e87d668600227502c15a16e8e8
Nicholas Simmons <triskele37@gmail.com>
2025-04-15 23:46:08 -0500

    fix page flow

--- src/app/mod-view/layout.jsx
+++ src/app/mod-view/layout.jsx
@@ -5,7 +5,7 @@ import { ModNavigation } from "@/components/ModView/ModNavigation/ModNavigation"
 export default function ModViewLayout({ children }) {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[25ch_1fr] max-w-screen-2xl mx-auto py-4">
-      <div className="top-[5.5rem] self-start hidden xl:block sticky h-[calc(100vh-100px)] scrollbar">
+      <div className="scrollbar">
         <ModNavigation />
       </div>
       <article className="min-h-screen px-6">{children}</article>
