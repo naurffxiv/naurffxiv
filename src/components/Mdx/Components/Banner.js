365abea508222c4edc0f269832bbe26968930f86
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-04-17 22:54:32 -0500

    update mdx page banner images

--- src/components/Mdx/Components/Banner.js
+++ src/components/Mdx/Components/Banner.js
@@ -3,9 +3,9 @@ import clsx from "clsx";
 
 export default function Banner({ src, alt, left = false, ...props }) {
   return (
-    <div className="relative h-48 not-prose">
+    <div className="relative h-56 not-prose">
       <Image
-        className={clsx("object-cover", { "object-left": left })}
+        className={clsx("object-cover rounded-sm", { "object-left": left })}
         src={src}
         alt={alt}
         {...props}
