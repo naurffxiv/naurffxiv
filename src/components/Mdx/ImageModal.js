d55783aaecc1b23db4c80150efbc64b8748af303
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-04-17 23:18:19 -0500

    Image modal adjustments (#202)
    Closes #201 
    Currently, image modals are way too small on mobile devices:
    
    ![image](https://github.com/user-attachments/assets/29187d6d-e318-48ce-9713-5fa736f132b1)
    
    This PR makes it so that it takes up the majority of the width like so:
    
    ![image](https://github.com/user-attachments/assets/b14d6432-0aa3-42f6-a8ac-6dae477ad73e)

--- src/components/Mdx/ImageModal.js
+++ src/components/Mdx/ImageModal.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Modal from "@mui/material/Modal";
 import Image from "next/image";
 
+// we can disable warnings for no alt-text as it will be provided by props
 export default function ImageModal({ src, compressedExt = "avif", ...props }) {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
@@ -12,6 +13,7 @@ export default function ImageModal({ src, compressedExt = "avif", ...props }) {
   return (
     <>
       <button onClick={handleOpen} className="not-prose">
+        {/* eslint-disable-next-line jsx-a11y/alt-text */}
         <Image src={compressed} {...props} />
       </button>
       <Modal
@@ -20,7 +22,8 @@ export default function ImageModal({ src, compressedExt = "avif", ...props }) {
         aria-labelledby={props.alt}
         className="not-prose"
       >
-        <div className="image-modal">
+        <div className="absolute top-1/2 left-[8%] -translate-x-[4%] xl:left-1/2 xl:-translate-x-1/2 -translate-y-1/2 shadow-[24] p-1">
+          {/* eslint-disable-next-line jsx-a11y/alt-text */}
           <Image src={src} {...props} />
         </div>
       </Modal>
