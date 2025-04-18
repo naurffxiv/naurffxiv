c86b3d325975d221c0fec838e34e3eaa10591119
memento-was-taken <170650581+memento-was-taken@users.noreply.github.com>
2025-04-17 23:22:16 -0400

    Display current ultimate, savage tier, and extreme trial on homepage (#196)

---
+++ src/components/Homepage/FightGuideComponent.js
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import clsx from "clsx";
+
+export default function FightGuideComponent({ entries, title, left }) {
+  return (
+    <div>
+      <h3 className="mt-12 text-2xl text-center">{title}</h3>
+      <div
+        className={clsx(
+          "mt-8 grid gap-x-10 gap-y-4 mx-auto",
+          entries.length === 1
+            ? "grid-cols-1 justify-center"
+            : "grid-cols-1 sm:grid-cols-2",
+        )}
+      >
+        {entries.map((entry) => (
+          <div
+            key={entry.url}
+            className="bg-[#10242b] overflow-hidden rounded-lg group"
+          >
+            <a href={entry.url}>
+              <div className="relative h-48 group-hover:brightness-75">
+                <Image
+                  src={entry.img}
+                  fill
+                  alt={entry.alt}
+                  className={clsx("object-cover", {
+                    "object-left": left,
+                    "object-center": !left,
+                  })}
+                />
+              </div>
+              <p className="px-3 py-2 text-lg font-medium text-center group-hover:underline">
+                {entry.title}
+              </p>
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
