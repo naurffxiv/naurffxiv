5bfdc4e31bf9dbbbd1a0ba64bba741cd986b2bc1
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-04-17 22:44:21 -0500

    add eslint rule exception to imagemodal

--- src/components/Mdx/ImageModal.js
+++ src/components/Mdx/ImageModal.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Modal from "@mui/material/Modal";
 import Image from "next/image";
 
+// we can disable warnings for no alt-text as it will be provided by props
 export default function ImageModal({ src, compressedExt = "avif", ...props }) {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
@@ -12,6 +13,7 @@ export default function ImageModal({ src, compressedExt = "avif", ...props }) {
   return (
     <>
       <button onClick={handleOpen} className="not-prose">
+        {/* eslint-disable-next-line jsx-a11y/alt-text */}
         <Image src={compressed} {...props} />
       </button>
       <Modal
@@ -21,6 +23,7 @@ export default function ImageModal({ src, compressedExt = "avif", ...props }) {
         className="not-prose"
       >
         <div className="absolute top-1/2 left-[8%] -translate-x-[4%] xl:left-1/2 xl:-translate-x-1/2 -translate-y-1/2 shadow-[24] p-1">
+          {/* eslint-disable-next-line jsx-a11y/alt-text */}
           <Image src={src} {...props} />
         </div>
       </Modal>
