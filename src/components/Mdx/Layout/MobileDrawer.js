3c0836b1fb76af139fe7ddcfd0e06d596ec567e1
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-04-19 00:33:54 -0500

    add mobile drawer exit button

--- src/components/Mdx/Layout/MobileDrawer.js
+++ src/components/Mdx/Layout/MobileDrawer.js
@@ -1,5 +1,6 @@
-import { Drawer, IconButton } from "@mui/material";
+import { Drawer, IconButton, Box } from "@mui/material";
 import { MenuOpen } from "@mui/icons-material";
+import ClearIcon from "@mui/icons-material/Clear";
 import React from "react";
 
 function MobileDrawer({ children }) {
@@ -16,21 +17,38 @@ function MobileDrawer({ children }) {
         anchor="right"
         slotProps={{ paper: { sx: { backgroundColor: "#1A3549" } } }}
       >
+        <Box sx={sx.closeDrawerContainer}>
+          <IconButton
+            sx={sx.closeDrawer}
+            size="large"
+            onClick={toggleDrawer(false)}
+          >
+            <ClearIcon sx={{ color: "white" }} />
+          </IconButton>
+        </Box>
         {children}
       </Drawer>
-      <IconButton
-        sx={{
-          background: "#28506E",
-          ":hover": { background: "#1A3549" },
-          marginBottom: "4rem",
-        }}
-        onClick={() => setOpen(true)}
-        size="large"
-      >
+      <IconButton sx={sx.openDrawer} onClick={toggleDrawer(true)} size="large">
         <MenuOpen sx={{ color: "white" }} />
       </IconButton>
     </div>
   );
 }
+
+const sx = {
+  openDrawer: {
+    background: "#28506E",
+    ":hover": { background: "#1A3549" },
+    marginBottom: "4rem",
+  },
+  closeDrawerContainer: {
+    display: "flex",
+    justifyContent: "flex-end",
+    padding: "0.5rem",
+  },
+  closeDrawer: {
+    ":hover": { background: "#00171fa3" },
+  },
+};
 
 export default MobileDrawer;
