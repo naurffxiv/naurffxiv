197dd1a13c7ab01ea00bf21f5c4b39d6cfab2d55
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-04-20 16:42:33 -0500

    Adjust position of mobile open quick links drawer button (#212)
    Closes #211 
    This PR adjusts the position of the mobile quick links drawer button
    slightly so that it is not cut off on mainstream mobile devices.
    
    ![image](https://github.com/user-attachments/assets/93744a9a-cb5d-47dc-aca4-f40d7d8c0a47)

--- src/components/Mdx/Layout/MobileDrawer.js
+++ src/components/Mdx/Layout/MobileDrawer.js
@@ -23,7 +23,8 @@ function MobileDrawer({ children }) {
         sx={{
           background: "#28506E",
           ":hover": { background: "#1A3549" },
-          marginBottom: "4rem",
+          marginRight: "0.75rem",
+          marginBottom: "5.75rem",
         }}
         onClick={() => setOpen(true)}
         size="large"
