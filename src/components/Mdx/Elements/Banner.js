1d8fe080f6b962327759862869cf8436f8028aeb
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-04-20 15:51:35 -0500

    change ultimate banners back

--- src/components/Mdx/Elements/Banner.js
+++ src/components/Mdx/Elements/Banner.js
@@ -3,7 +3,7 @@ import clsx from "clsx";
 
 export default function Banner({ src, alt, left = false, ...props }) {
   return (
-    <div className="relative h-56 not-prose">
+    <div className="relative h-48 lg:h-56 not-prose">
       <Image
         className={clsx("object-cover rounded-sm", { "object-left": left })}
         src={src}
