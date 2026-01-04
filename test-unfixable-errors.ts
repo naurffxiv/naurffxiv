5643db2227e0a8d1c695490e504a05cd8e0897e7
Luna <Luna.dev.97@gmail.com>
2026-01-04 16:50:39 -0500

    test: unfixable errors

---
+++ test-unfixable-errors.ts
@@ -0,0 +1,9 @@
+// unfixable error: unused variable
+const unusedVariable = "this will cause an error";
+
+// unfixable error: implicit any type
+function badFunction(param) {
+  return param;
+}
+
+export default badFunction;
