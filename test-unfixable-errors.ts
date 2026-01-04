1c243e4ef14e713ffceaac3a91d2d9756312b129
Luna <Luna.dev.97@gmail.com>
2026-01-04 16:51:37 -0500

    test: remove test files

--- test-unfixable-errors.ts
+++
@@ -1,9 +0,0 @@
-// unfixable error: unused variable
-const unusedVariable = "this will cause an error";
-
-// unfixable error: implicit any type
-function badFunction(param) {
-  return param;
-}
-
-export default badFunction;
